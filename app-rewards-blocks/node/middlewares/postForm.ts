79f8b89707a0e9745e7a4a733a33457e12b4e28c
Wellington Wanderosfky <wellingtonwpedro@gmail.com>
2025-07-08 22:36:52 -0300

    fix: post form

--- app-rewards-blocks/node/middlewares/postForm.ts
+++ app-rewards-blocks/node/middlewares/postForm.ts
@@ -1,21 +1,10 @@
 export async function postForm(ctx: Context, next: () => Promise<any>) {
-  const { clients: { guper, session }, vtex: { sessionToken } } = ctx
+  const { clients: { guper } } = ctx
 
+  const response = await guper.postForm(ctx.state.body.submissionKey, ctx.state.body.data)
+  ctx.body = response.data
+  ctx.status = 200;
 
-  const sessionInfo = sessionToken ? await session.getSession(sessionToken, ["*"]) : null
+  await next()
 
-  const email = sessionInfo?.sessionData?.namespaces?.profile?.email?.value
-
-  if(!email) {
-    ctx.body = {}
-    ctx.status = 401;
-
-    await next()
-  } else {
-    const response = await guper.postForm(ctx.state.body.submissionKey, ctx.state.body.data)
-    ctx.body = response.data
-    ctx.status = 200;
-
-    await next()
-  }
 }
